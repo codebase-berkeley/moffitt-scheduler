7219916f76ab4ed13d843c6dd2a340653dd53246
Sahil Thakur <sahilthakur@berkeley.edu>
2020-02-17 22:22:51 -0800

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Employees from './components/Employees/Employees'
 
 import './App.css'
 
@@ -17,6 +18,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/employees" component={Employees} />
         </Switch>
       </BrowserRouter>
     )
---
+++ client/src/components/Employees/Employees.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Employees(){
+    return (
+        <h1>Employees</h1>
+    )
+}
