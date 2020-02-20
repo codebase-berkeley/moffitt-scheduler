6645e9c34740a1a801c421535f8869013910a5da
Kerry Huang <kerryhuang@berkeley.edu>
2020-02-19 20:18:13 -0800

    Deleted starters in public; Changed class to function in App.js; Added blank Calendar component

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,27 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Calendar from './components/Calendar/Calendar';
 
 import './App.css'
 
-class App extends React.Component {
+/*class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  render() {
+  render()*/ 
+  
+  function App(){
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/calendar" component={Calendar}/>
         </Switch>
       </BrowserRouter>
     )
   }
-}
 
 export default App;
