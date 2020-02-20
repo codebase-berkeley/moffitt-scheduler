aa6588def11b94833af8e0dac90939df5fa7946f
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-02-19 23:06:50 -0800

    added announcements

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Announcement from './components/Announcement/Announcement';
+import Employee from './components/Employee/Employee';
+import Cover from './components/Cover/Cover';
+
 
 import './App.css'
 
@@ -19,6 +22,8 @@ class App extends React.Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/announcement" component={Announcement} />
+          <Route exact path="/employees" component={Employee} />
+          <Route exact path="/cover" component={Cover} />
         </Switch>
       </BrowserRouter>
     )
---
+++ client/src/components/Cover/Cover.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Cover.css";
+
+export default function Cover() {
+    return (
+        <div className="all">
+            <p>hello world!</p>
+        </div>
+    );
+}
