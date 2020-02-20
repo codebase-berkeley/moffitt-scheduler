00d31f967912e727dd1a19cbb6fb59bf168a957c
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-02-19 23:23:45 -0800

    finished announcements page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Employees from './components/Employees/Employees';
+import Announcement from './components/Announcement/Announcement';
 
 import './App.css'
 
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/employees" component={Employees} />
+          <Route exact path="/announcement" component={Announcement} />
         </Switch>
       </BrowserRouter>
     )
