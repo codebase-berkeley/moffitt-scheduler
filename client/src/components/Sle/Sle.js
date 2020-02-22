c73dd5b7f84a14a3ef34f9ef2e523e1779cf88a1
Kathleen Kong <kathleenkong@berkeley.edu>
2020-02-22 15:07:42 -0800

    SLE view v2

--- client/src/components/Sle/Sle.js
+++ client/src/components/Sle/Sle.js
@@ -17,10 +17,6 @@ function Sle(props) {
         <div id="circle"></div> */}
       </div>
 
-      <div class = "name">
-        <p>&nbsp;&nbsp;Bianca Lee &nbsp;&nbsp; <i class="arrow"></i></p>
-
-    </div>
 
     <div class = "sidebar">
       <Sidebar
@@ -31,26 +27,25 @@ function Sle(props) {
           title= "Availability"/>
     </div>
 
-    <div class = "rectangle"> </div>
-
-    <div class="user-box">
-  <div class="user-id">
-    <img class="user-picture" src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash3/c34.34.432.432/s160x160/575695_10101114843671310_1323509961_n.jpg"/>
-    <div class="user-name">
-      DU7S
-    </div>
-    <div class="dropdown-arrow"></div>
-    <div class="dropdown-menu">
-      <ul>
-        <li>Settings</li>
-        <li>Sports</li>
-        <li>Log Out</li>
-      </ul>
+    <div class = "top-right">
+      <div class="user-box">
+      <div class="user-id">
+      <div class="user-name"> Bianca Lee
+      </div>
+      <div class="dropdown-arrow"></div>
+      <div class="dropdown-menu">
+        <ul>
+          <li>View Profile</li>
+          <li>Log Out</li>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
+  </div>
 
-    </div>
+  <div class = "rectangle"> </div>
+
+</div>
 
     
   );
