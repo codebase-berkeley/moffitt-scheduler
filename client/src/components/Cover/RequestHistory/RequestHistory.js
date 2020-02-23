0d40712822d11670c168d66c648932de1f8199de
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-02-22 21:55:51 -0800

    finishedpendingcoveragepage

---
+++ client/src/components/Cover/RequestHistory/RequestHistory.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./RequestHistory.css";
+
+export default function RequestHistory() {
+    return (
+        <div className="all">
+            <p>hello world!!</p>
+        </div>
+    );
+}
