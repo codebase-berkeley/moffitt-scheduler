024b8862de6535e145aa65276070b3f975670f64
Sahil Thakur <sahilthakur@berkeley.edu>
2020-02-22 20:35:53 -0800

    CIRCLES DONT WORK

--- client/src/components/Employees/EachEmployee.jsx
+++ client/src/components/Employees/EachEmployee.jsx
@@ -4,16 +4,16 @@ import "./EachEmployee.css";
 export default function EachEmployee(props) {
     return (
     <div className="someEmployee">
-        <div className="textFormat">
-            <div class="sphere"></div>
+        <div className="pleaseWork">
+            <div class="holder"></div>
             <div className="vertical">
                 <h2 className="name">{props.name}</h2>
                 <h3 className="email">{props.email}</h3>
-            </div>
+        </div>
+        </div>
             <div className="lib">
-                <h2 className="first">{props.firstLibrary}</h2>
-                <h2 className="second">{props.secondLibrary}</h2>
-            </div>
+                <div className="first">{props.firstLibrary}</div>
+                <div className="second">{props.secondLibrary}</div>
             </div>
         
     </div>
