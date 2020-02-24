d3292d9a2ce2a15db5d40bd4c93553c7af66042a
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-02-23 20:30:24 -0800

    Corrected some spacing issues

--- client/src/components/Cover/PendingCov.jsx
+++ client/src/components/Cover/PendingCov.jsx
@@ -3,23 +3,23 @@ import "./PendingCov.css";
 
 export default function PendingCov(props) {
     return (
-        <div className="shift">
-            <div className="time_loc">
-                <div className="firstrow">
-                    <div className="bold_desk">
+        <div className="shift1">
+            <div className="time_loc1">
+                <div className="firstrow1">
+                    <div className="bold_desk1">
                         <p className="desk">{props.desk}</p>
                     </div>
-                    <div className="colorful_box">
+                    <div className="colorful_box1">
                         <p className="loc">{props.loc}</p>
                     </div>
                 </div>
-                <p className="date">{props.date}</p>
+                <p className="date1">{props.date}</p>
                 <p className="time">{props.time}</p>
             </div>
-            <div className="need_cov">
+            <div className="need_cov1">
                 <p className="needname">{props.needname}</p>
             </div>
-            <div className="notes">
+            <div className="notes1">
                 <p className="message">{props.message}</p>
             </div>
         </div>
