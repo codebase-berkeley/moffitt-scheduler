6f79f03e0e5c2681e22e49a10794ab0d488013e7
Kerry Huang <kerryhuang@berkeley.edu>
2020-02-25 13:41:13 -0800

    Remade Scheduler module from scratch for staticcalendar

--- client/src/components/Calendar/StaticCalendar.js
+++ client/src/components/Calendar/StaticCalendar.js
@@ -1,32 +1,270 @@
 import React from "react";
 import "./StaticCalendar.css";
-import { render } from "@testing-library/react";
-import { format, startOfWeek, endOfWeek } from "date-fns";
-
+import { format, startOfWeek, endOfWeek, addDays } from "date-fns";
 
 export default class StaticCalendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { schedule: [] };
 
-    constructor(props) {
-        super(props);
+    this.currentDate = new Date();
+    this.weekString =
+      format(this.currentDate, "MMMM") +
+      " " +
+      format(this.currentDate, "YYYY") +
+      ": " +
+      format(startOfWeek(this.currentDate), "MM/DD") +
+      " - " +
+      format(endOfWeek(this.currentDate), "MM/DD");
+  }
 
-        this.currentDate = new Date();
-        this.weekString =
-            format(this.currentDate, "MMMM") +
-            " " +
-            format(this.currentDate, "YYYY") +
-            ": " +
-            format(startOfWeek(this.currentDate), "MM/DD") +
-            " - " +
-            format(endOfWeek(this.currentDate), "MM/DD");
-    }
-
-    render() {
-        return (
-            <div id="overall-container">
-                <div class="schedule-container">
-                    <h1 id="weekString">{this.weekString}</h1>
-                </div>
+  render() {
+    return (
+      <div id="overall-container">
+        <div id="schedule-container-st">
+          <h1 id="weekString">{this.weekString}</h1>
+          <div id="inner-schedule">
+            <div></div>
+            <div class="item-wday">
+              {format(startOfWeek(this.currentDate), "dd MM/DD")}
             </div>
-        )
-    }
+            <div class="item-wday">
+              {format(addDays(startOfWeek(this.currentDate), 1), "dd MM/DD")}
+            </div>
+            <div class="item-wday">
+              {format(addDays(startOfWeek(this.currentDate), 2), "dd MM/DD")}
+            </div>
+            <div class="item-wday">
+              {format(addDays(startOfWeek(this.currentDate), 3), "dd MM/DD")}
+            </div>
+            <div class="item-wday">
+              {format(addDays(startOfWeek(this.currentDate), 4), "dd MM/DD")}
+            </div>
+            <div class="item-wday">
+              {format(addDays(startOfWeek(this.currentDate), 5), "dd MM/DD")}
+            </div>
+            <div class="item-wday">
+              {format(endOfWeek(this.currentDate), "dd MM/DD")}
+            </div>
+
+            <div class="item-hours">12am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">1am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">2am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">3am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">4am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">5am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">6am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">7am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">8am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">9am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">10am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">11am</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">12pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">1pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">2pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">3pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">4pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">5pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">6pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">7pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">8pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">9pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">10pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+
+            <div class="item-hours">11pm</div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+            <div class="item-cell"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
