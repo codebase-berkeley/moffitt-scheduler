cfb05e1b35b7ddb20a6a45cc5c97b10ba0164f3e
Kerry Huang <kerryhuang@berkeley.edu>
2020-02-24 16:38:06 -0800

    Removed package2.json; reverted index.js. Extraneous comments are still there

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calendar from "./components/Calendar/Calendar";
 
-function App() {
-    return (
-      <div className="App">
-        <Calendar/>
-      </div>
-    );
-  }
+import App from "./App.js"
 
 ReactDOM.render(
     <App />, 
