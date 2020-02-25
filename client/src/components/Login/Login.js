58544285e1d9305bd2f29360eb0f9624fa36a875
unknown <tetsuroescudero@berkeley.edu>
2020-02-24 23:06:10 -0800

    login v2

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,59 +1,48 @@
-import React from 'react';
-import "./Login.css"
+import React from "react";
+import "./Login.css";
 
 function Login(props) {
-    return (
-        <div className='wholePage'>
-        <div className='loginBox'>
-            <div className='loginBoxUpperPart'>
-             <div className='login'>
-                <h1>Log In</h1>
-             </div>
-            </div>
-            <div className='loginBoxLowerPart'>
-
-            </div>
-            <div className='emailBox'>
-                <div className='email'>
-                <form>
-                     <input type="text" placeholder="Email"></input>
-                </form>
-
-                </div>
-
-            </div>
-            <div className='passwordBox'>
-                <div className='password'>
-                <form>
-                     {/* <label for="fname">Password</label> */}
-                     <input type="text" placeholder="Password"></input>
-                </form>
-
-                    {/* <h3>Password</h3> */}
-
-                </div>
-
-            </div>
-            <div className='logInButton'>
-                <div className='logInButtonText'>
-                    <h2>Log In</h2>
-
-                </div>
-
-            </div>
-            <div className='createAccountButton'>
-                <div className='createAccountButtonText'>
-                    <h3>Create Account</h3>
-
-                </div>
-
-            </div>
+  return (
+    <div className="wholePage">
+      <div className="loginBox">
+        <div className="loginBoxUpperPart">
+          <div className="login">
+            <h1>Log In</h1>
+          </div>
         </div>
-        <div className='loginBoxSurroundings'>
-
+        <div className="loginBoxLowerPart">
+          <div className="emailBox">
+            <div className="email">
+              <form>
+                <input type="text" placeholder="Email"></input>
+              </form>
+            </div>
+          </div>
+          <div className="passwordBox">
+            <div className="password">
+              <form>
+                <input type="text" placeholder="Password"></input>
+              </form>
+            </div>
+          </div>
+          <div className="button-container">
+            <div className="logInButton">
+              <div className="logInHover">
+                <div className="logInButtonText">
+                  <h4>Log In</h4>
+                </div>
+              </div>
+            </div>
+            <div className="createAccountButton">
+              <div className="createAccountButtonText">
+                <h4>Create Account</h4>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+      <div className="loginBoxSurroundings"></div>
+    </div>
+  );
 }
-
 export default Login;
