0ce9fd7098d7cc45f48bbaeaad75b4b4dd713cd5
Sahil Thakur <sahilthakur@berkeley.edu>
2020-02-24 22:10:25 -0800

    Fake database Cover

--- client/src/components/Cover/Cover.js
+++ client/src/components/Cover/Cover.js
@@ -2,6 +2,38 @@ import React from "react";
 import PendingCov from "./PendingCov";
 import "./Cover.css";
 
+const database = [
+  {
+    desk:"Front Desk", 
+    loc:"Moffitt", 
+    date:"Wednesday, March 6, 2020", 
+    time: "3:00 PM - 5:00 PM",
+    needname: "Broco Lee",
+    message: "Going home for the weekend",
+  },
+  {
+    desk:"Front Desk", 
+    loc:"Moffitt", 
+    date:"Wednesday, March 6, 2020", 
+    time: "3:00 PM - 5:00 PM",
+    needname: "Broco Lee",
+    message: "Going home for the weekend",
+  }
+]
+function processData(database) {
+  const listItems = database.map((entry, index) =>
+    <PendingCov
+      desk={entry.desk}
+      loc={entry.loc}
+      date={entry.date}
+      time={entry.time}
+      needname={entry.needname}
+      message={entry.message}
+    />
+  );
+  return listItems;
+}
+
 export default function Cover() {
   return (
     <div className="all">
@@ -16,7 +48,8 @@ export default function Cover() {
         <h2 className="msame02">Notes</h2>
         <h2 className="msame03"></h2>
       </div>
-      <PendingCov
+      {processData(database)}
+      {/* <PendingCov
         desk="Front Desk"
         loc="Moffitt"
         date="Wednesday, March 6, 2020"
@@ -38,7 +71,8 @@ export default function Cover() {
         date="Friday, March 8, 2020"
         time="2:00 PM - 4:00 PM"
         needname="Fami Lee"
-      />
+      /> */}
     </div>
   );
 }
+
