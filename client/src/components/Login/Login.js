f1968ceb8607a78e2b850de1749605d6f33b9bb7
unknown <tetsuroescudero@berkeley.edu>
2020-02-27 22:14:42 -0800

    login with backend v1

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,48 +1,66 @@
 import React from "react";
 import "./Login.css";
 
-function Login(props) {
-  return (
-    <div className="wholePage">
-      <div className="loginBox">
-        <div className="loginBoxUpperPart">
-          <div className="login">
-            <h1>Log In</h1>
-          </div>
-        </div>
-        <div className="loginBoxLowerPart">
-          <div className="emailBox">
-            <div className="email">
-              <form>
-                <input type="text" placeholder="Email"></input>
-              </form>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="wholePage">
+        <div className="loginBox">
+          <div className="loginBoxUpperPart">
+            <div className="login">
+              <h1>Log In</h1>
             </div>
           </div>
-          <div className="passwordBox">
-            <div className="password">
-              <form>
-                <input type="text" placeholder="Password"></input>
-              </form>
-            </div>
-          </div>
-          <div className="button-container">
-            <div className="logInButton">
-              <div className="logInHover">
-                <div className="logInButtonText">
-                  <h4>Log In</h4>
-                </div>
+          <div className="loginBoxLowerPart">
+            <div className="emailBox">
+              <div className="email">
+                <input type="text" placeholder="Email" id="email"></input>
               </div>
             </div>
-            <div className="createAccountButton">
-              <div className="createAccountButtonText">
-                <h4>Create Account</h4>
+            <div className="passwordBox">
+              <div className="password">
+                <input type="text" placeholder="Password" id="password"></input>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="loginBoxSurroundings"></div>
-    </div>
-  );
+            <div className="button-container">
+                <button className="logInButton" onClick={this.loginClick} >Log In</button>
+              <button className="createAccountButton" onClick={this.createAccountClick}>Create Account</button>
+              </div>  
+            </div>
+          /</div>
+        /</div>
+    );
+  }
+
+  loginClick() {
+    console.log("In click function");
+    var email = document.getElementById("email");
+    var emailText = email.value;
+    console.log(emailText);
+    var password = document.getElementById("password");
+    var passwordText = password.value;
+    console.log(passwordText);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email: emailText, password: passwordText })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        console.log(jsonResponse);
+      });
+  }
+  createAccountClick(e) {
+    console.log("hi");
+  }
 }
+
 export default Login;
--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-var exampleRoutes = require('./routes/example');
+// var exampleRoutes = require("./routes/example");
 
-var cors = require('cors');
+var loginRoutes = require("./routes/login");
 
-app.use(bodyParser.urlencoded({extended: true}));
+var cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/example', exampleRoutes);
+app.use("/", loginRoutes);
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(8000, () => { console.log("Listening on port 8000")});
+app.listen(8000, () => {
+  console.log("Listening on port 8000");
+});
