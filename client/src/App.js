a44e5b3602f77cd0dc0067ca7a73febadd294f77
tetsuroescudero <60587277+tetsuroescudero@users.noreply.github.com>
2020-03-02 13:04:41 -0800

    Login with backend (#15)

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Calendar from './components/Calendar/Calendar';
-import StaticCalendar from './components/Calendar/StaticCalendar';
-import Employees from './components/Employees/Employees';
-import Cover from './components/Cover/Cover';
-import PendingSupervisor from './components/Cover/PendingSupervisor/PendingSupervisor';
-import RequestHistory from './components/Cover/RequestHistory/RequestHistory';
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Calendar from "./components/Calendar/Calendar";
+import StaticCalendar from "./components/Calendar/StaticCalendar";
+import Employees from "./components/Employees/Employees";
+import Cover from "./components/Cover/Cover";
+import PendingSupervisor from "./components/Cover/PendingSupervisor/PendingSupervisor";
+import RequestHistory from "./components/Cover/RequestHistory/RequestHistory";
 import Sle from "./components/Sle/Sle";
 import Supervisor from "./components/Supervisor/Supervisor";
+import addEmployees from "./components/Employees/addEmployees";
 
 import "./App.css";
 
@@ -28,10 +29,19 @@ class App extends React.Component {
           <Route exact path="/staticCalendar" component={StaticCalendar} />
           <Route exact path="/employees" component={Employees} />
           <Route exact path="/cover" component={Cover} />
-          <Route exact path="/cover/pendingsupervisor" component={PendingSupervisor} />
-          <Route exact path="/cover/requesthistory" component={RequestHistory} />
+          <Route
+            exact
+            path="/cover/pendingsupervisor"
+            component={PendingSupervisor}
+          />
+          <Route
+            exact
+            path="/cover/requesthistory"
+            component={RequestHistory}
+          />
           <Route exact path="/Sle" component={Sle} />
           <Route exact path="/Supervisor" component={Supervisor} />
+          <Route exact path="/addEmployees" component={addEmployees} />
         </Switch>
       </BrowserRouter>
     );
