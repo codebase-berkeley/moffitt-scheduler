552275d772c0ccd75d659c89703cfdc6a8d7d321
sahilthakur3029 <60587293+sahilthakur3029@users.noreply.github.com>
2020-03-02 13:11:30 -0800

    Refactor and add backend to cover requests

--- client/src/components/Cover/WithCheck.js
+++ client/src/components/Cover/WithCheck.js
@@ -1,33 +1,80 @@
 import React from "react";
 import "./WithCheck.css";
-import check from "../check.png";
-import cross from "../cross.png";
+import checkNofill from "./Images/check_nofill.svg";
+import denyNofill from "./Images/deny_nofill.svg";
 
-export default function WithCheck(props) {
-  return (
-    <div className="shift">
-      <div className="time_loc">
-        <div className="firstrow">
-          <div className="bold_desk">
-            <p className="desk">{props.desk}</p>
+class WithCheck extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="shift">
+        <div className="time_loc">
+          <div className="firstrow">
+            <div className="bold_desk">
+              <p className="desk">{this.props.desk}</p>
+            </div>
+            <div className="colorful_box">
+              <p className="loc">{this.props.loc}</p>
+            </div>
           </div>
-          <div className="colorful_box">
-            <p className="loc">{props.loc}</p>
-          </div>
+          <p className="date">{this.props.date}</p>
+          <p className="time">{this.props.time}</p>
         </div>
-        <p className="date">{props.date}</p>
-        <p className="time">{props.time}</p>
+        <div className="need_cov">
+          <p className="needname">{this.props.needname}</p>
+        </div>
+        <div className="covering">
+          <p className="covername">{this.props.covername}</p>
+        </div>
+        <div className="approval">
+          <button className="checkbutton" onClick={this.approvalClick}>
+            <img className="check_nofill" src={checkNofill} alt="check" />
+          </button>
+          <button className="denybutton" onClick={this.denialClick}>
+            <img className="deny_nofill" src={denyNofill} alt="deny" />
+          </button>
+        </div>
       </div>
-      <div className="need_cov">
-        <p className="needname">{props.needname}</p>
-      </div>
-      <div className="covering">
-        <p className="covername">{props.covername}</p>
-      </div>
-      <div className="approval">
-        <img className="check" src={check} alt="check" />
-        <img className="deny" src={cross} alt="deny" />
-      </div>
-    </div>
-  );
+    );
+  }
+
+  approvalClick(e) {
+    console.log("In click function");
+    fetch("/pendingsupervisor", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ approve: true })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        console.log(jsonResponse);
+      });
+  }
+
+  denialClick(e) {
+    console.log("In click function");
+    fetch("/pendingsupervisor", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ approve: false })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        console.log(jsonResponse);
+      });
+  }
 }
+export default WithCheck;
--- server/app.js
+++ server/app.js
@@ -2,19 +2,19 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
-// var exampleRoutes = require("./routes/example");
-
-var loginRoutes = require("./routes/login");
+// var exampleRoutes = require('./routes/example');
+var pendingSupervisor = require("./routes/coverrequests");
 
 var cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use("/", pendingSupervisor);
 app.use("/", loginRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello world!");
 });
 
 app.listen(8000, () => {
