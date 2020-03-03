1de59f10cc37b82c68f31e3cf6e6f5be6cddff1d
unknown <tetsuroescudero@berkeley.edu>
2020-03-02 23:01:47 -0800

    saveemployees

---
+++ dbtest.js
@@ -0,0 +1,17 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "miW478facabbghy78",
+  host: "127.0.0.1",
+  database: "moffitt",
+  port: 5432
+});
+
+pool.query("SELECT * FROM sle", (error, result) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(result);
+});
--- server/routes/employees.js
+++ server/routes/employees.js
@@ -1,6 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "miW478facabbghy78",
+  host: "127.0.0.1",
+  database: "moffitt",
+  port: 5432
+});
+
 router.post("/employees", function(req, res) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
@@ -8,7 +18,19 @@ router.post("/employees", function(req, res) {
   console.log("lastName", lastName);
   console.log("working");
   res.json({ Successful: true });
-  // res.send("Hi!");
+
+  const text =
+    "INSERT INTO sle (name, training_level_doe, training_level_moffitt, email, password) VALUES ($1, $2, $3, $4, $5)";
+  const values = [firstName, 1, 1, null, null];
+
+  pool.query(text, values, (error, result) => {
+    if (error) {
+      // throw error;
+      console.log(error);
+    }
+    console.log(result);
+    console.log("insidequery");
+  });
 });
 
 // router.get("/age", function(req, res) {
@@ -17,3 +39,8 @@ router.post("/employees", function(req, res) {
 // });
 
 module.exports = router;
+
+// pool.query("SELECT * FROM sle", (error, result) => {
+//   if (error) {
+//     throw error;
+//   }
