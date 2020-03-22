1a8ed14ddedf34e08aea9da374003122912e1948
Sahil Thakur <sahilthakur@berkeley.edu>
2020-03-22 14:33:00 -0700

    2D array implementation

--- client/src/components/MasterSchedule/Doe.js
+++ client/src/components/MasterSchedule/Doe.js
@@ -2,19 +2,42 @@ import React from "react";
 import "./Doe.css";
 
 export default function Doe(props) {
-  var array = [];
+  var [
+    sundayArray,
+    mondayArray,
+    tuesdayArray,
+    wednesdayArray,
+    thursdayArray,
+    fridayArray,
+    saturdayArray
+  ] = [[], [], [], [], [], [], []];
+  var allDaysOfWeek = [
+    sundayArray,
+    mondayArray,
+    tuesdayArray,
+    wednesdayArray,
+    thursdayArray,
+    fridayArray,
+    saturdayArray
+  ];
   for (var i = 0; i < 6; i++) {
-    array.push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[0].push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[1].push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[2].push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[3].push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[4].push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[5].push(<Box text="Tetsu Escudero" />);
+    allDaysOfWeek[6].push(<Box text="Tetsu Escudero" />);
   }
   return (
     <div className="weekdayColumns">
-      <div className="sundayColumn">{array}</div>
-      <div className="mondayColumn">{array}</div>
-      <div className="tuesdayColumn">{array}</div>
-      <div className="wednesdayColumn">{array}</div>
-      <div className="thursdayColumn">{array}</div>
-      <div className="fridayColumn">{array}</div>
-      <div className="saturdayColumn">{array}</div>
+      <div className="sundayColumn">{sundayArray}</div>
+      <div className="mondayColumn">{mondayArray}</div>
+      <div className="tuesdayColumn">{tuesdayArray}</div>
+      <div className="wednesdayColumn">{wednesdayArray}</div>
+      <div className="thursdayColumn">{thursdayArray}</div>
+      <div className="fridayColumn">{fridayArray}</div>
+      <div className="saturdayColumn">{saturdayArray}</div>
     </div>
   );
 }
--- client/src/components/MasterSchedule/Moffitt.js
+++ client/src/components/MasterSchedule/Moffitt.js
@@ -2,19 +2,42 @@ import React from "react";
 import "./Moffitt.css";
 
 export default function Moffitt(props) {
-  var array = [];
-  for (var i = 0; i < 5; i++) {
-    array.push(<Box text="Sahil Thakur" />);
+  var [
+    sundayArray,
+    mondayArray,
+    tuesdayArray,
+    wednesdayArray,
+    thursdayArray,
+    fridayArray,
+    saturdayArray
+  ] = [[], [], [], [], [], [], []];
+  var allDaysOfWeek = [
+    sundayArray,
+    mondayArray,
+    tuesdayArray,
+    wednesdayArray,
+    thursdayArray,
+    fridayArray,
+    saturdayArray
+  ];
+  for (var i = 0; i < 6; i++) {
+    allDaysOfWeek[0].push(<Box text="Sahil Thakur" />);
+    allDaysOfWeek[1].push(<Box text="Sahil Thakur" />);
+    allDaysOfWeek[2].push(<Box text="Sahil Thakur" />);
+    allDaysOfWeek[3].push(<Box text="Sahil Thakur" />);
+    allDaysOfWeek[4].push(<Box text="Sahil Thakur" />);
+    allDaysOfWeek[5].push(<Box text="Sahil Thakur" />);
+    allDaysOfWeek[6].push(<Box text="Sahil Thakur" />);
   }
   return (
     <div className="weekdayColumns">
-      <div className="sundayColumn">{array}</div>
-      <div className="mondayColumn">{array}</div>
-      <div className="tuesdayColumn">{array}</div>
-      <div className="wednesdayColumn">{array}</div>
-      <div className="thursdayColumn">{array}</div>
-      <div className="fridayColumn">{array}</div>
-      <div className="saturdayColumn">{array}</div>
+      <div className="sundayColumn">{sundayArray}</div>
+      <div className="mondayColumn">{mondayArray}</div>
+      <div className="tuesdayColumn">{tuesdayArray}</div>
+      <div className="wednesdayColumn">{wednesdayArray}</div>
+      <div className="thursdayColumn">{thursdayArray}</div>
+      <div className="fridayColumn">{fridayArray}</div>
+      <div className="saturdayColumn">{saturdayArray}</div>
     </div>
   );
 }
