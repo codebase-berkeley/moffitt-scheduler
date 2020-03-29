bbd19b86cc613bc020ca8dc121c486309f39f250
Katja Maucec <katja.maucec@berkeley.edu>
2020-03-29 14:59:28 -0600

    fixed pr comments

--- client/src/components/Sle/SleYourShifts.js
+++ client/src/components/Sle/SleYourShifts.js
@@ -41,35 +41,3 @@ export default class SleShifts extends React.Component {
   }
 }
 
-
-// function SleYourShifts(props) {
-//   return (
-//     <div class="everything">
-//       <div class="line"></div>
-//       <div className="top-bar">
-//         <div class="user-box">
-//           <div class="user-id">
-//             <div class="user-name">Bianca Lee</div>
-//             <div class="dropdown-arrow"></div>
-//             <div class="dropdown-menu">
-//               <ul>
-//                 <li>View Profile</li>
-//                 <li>Log Out</li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div class="sidebar">
-//         <SidebarElement title="Your Shifts" link="/yourshifts" />
-//         <SidebarElement title="Open Shifts" />
-//         <SidebarElement title="Availability" link="/availability" />
-//       </div>
-//       <div class="StaticCalendar">
-//         <StaticCalendar userId={this.props.match.params.userId} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default SleYourShifts;
