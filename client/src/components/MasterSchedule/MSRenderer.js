5456227621fbda4c9084ad6b0a73a6577e37c45e
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-04-10 00:56:19 -0700

    modal pops up

--- client/src/components/MasterSchedule/MSRenderer.js
+++ client/src/components/MasterSchedule/MSRenderer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "react-modal";
 import "./MSRenderer.css";
 import Moffitt from "./Moffitt";
 import Doe from "./Doe";
@@ -8,7 +9,7 @@ export default class MSRenderer extends React.Component {
     super(props);
     this.state = {
       items: [{}],
-      typeOfLibrary: "moffitt"
+      typeOfLibrary: "moffitt",
     };
     this.showMoffit = this.showMoffit.bind(this);
     this.showDoe = this.showDoe.bind(this);
@@ -49,6 +50,9 @@ export default class MSRenderer extends React.Component {
             <button className={nonClicked} onClick={this.showDoe}>
               <h1>Doe</h1>
             </button>
+          </div>
+          <div className="editButtonCont">
+            <AddEmployee />
           </div>
         </div>
 
@@ -101,4 +105,134 @@ export default class MSRenderer extends React.Component {
 
 function Box(props) {
   return <div>{props.text}</div>;
+}
+
+function AddEmployee() {
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#black";
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  var subtitle;
+
+  const customStyles = {
+    content: {
+      top: "400px",
+      left: "50%",
+      width: "450px",
+      height: "400px",
+      transform: "translate(-50%, -50%)",
+      overflow: 0,
+    },
+  };
+
+  function submitClick() {
+    var firstName = document.getElementById("firstName");
+    var firstNameText = firstName.value;
+    console.log(firstNameText);
+    var lastName = document.getElementById("lastName");
+    var lastNameText = lastName.value;
+    console.log(lastNameText);
+    var email = document.getElementById("email");
+    var emailText = email.value;
+    console.log(emailText);
+    fetch("http://localhost:8000/employees", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: firstNameText,
+        lastName: lastNameText,
+        email: emailText,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        console.log(jsonResponse);
+      });
+    function cancelClick() {
+      console.log("doesNothingForNow");
+    }
+  }
+
+  return (
+    <div>
+      <button className="editButton" onClick={openModal}>
+        Edit Schedule
+      </button>
+      <Modal
+        // className="box"
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className="AllText">
+          <h1
+            className="AddEmpText"
+            ref={(_subtitle) => (subtitle = _subtitle)}
+          >
+            Add Employee
+          </h1>
+          <div className="EmployeeName">
+            <h3 className="FirstName">First Name</h3>
+            <h3 className="LastName">Last Name</h3>
+            <input className="FirstNameInput" id="firstName" />
+            <input className="LastNameInput" id="lastName" />
+          </div>
+          <div className="EmployeeEmail">
+            <h3 className="Email">Email</h3>
+            <input className="EmailInput" id="email" />
+          </div>
+          <h3 className="TrainedFor">Trained For:</h3>
+          <div className="RadioButtons">
+            <h3 className="MoffittText">Moffitt</h3>
+            <div className="MoffittButton">
+              <input
+                type="checkbox"
+                id="moffitt"
+                name="moffitt"
+                value="moffitt"
+              />
+            </div>
+            <h3 className="DoeText">Doe</h3>
+            <div className="DoeButton">
+              <input type="checkbox" id="doe" name="doe" value="doe" />
+            </div>
+          </div>
+        </div>
+        <div className="button-container">
+          <a href="/employees">
+            <button className="CancelButton">
+              <div className="CancelHover">
+                <div className="CancelText">
+                  <h4> Cancel</h4>
+                </div>
+              </div>
+            </button>
+          </a>
+          <button className="SubmitButton" onClick={submitClick}>
+            <div className="SubmitText">
+              <h4>Submit</h4>
+            </div>
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
 }
