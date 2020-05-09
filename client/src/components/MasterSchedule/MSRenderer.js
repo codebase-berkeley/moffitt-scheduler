5ee7a9cd7c329c11c389b9e24f08f21974696a33
Stephanie Xiao <stephaniexiao@Stephanies-MacBook-Pro.local>
2020-05-08 17:48:55 -0700

    abbreviated months for formatting purposes

--- client/src/components/MasterSchedule/MSRenderer.js
+++ client/src/components/MasterSchedule/MSRenderer.js
@@ -17,18 +17,18 @@ function dateObject(day, hour) {
 
 function displayMonth(m) {
   const month = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
     5: "May",
     6: "June",
     7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
   };
   return month[m];
 }
